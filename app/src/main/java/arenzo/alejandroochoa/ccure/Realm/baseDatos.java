08b84569c9524dda20df00f8ee2d5f618b9aefbb
Alejandro Ochoa <alex.jym13@gmail.com>
2017-04-22 10:04:06 -0500

    inicio del proyecto

---
+++ app/src/main/java/arenzo/alejandroochoa/ccure/Realm/baseDatos.java
@@ -0,0 +1,16 @@
+package arenzo.alejandroochoa.ccure.Realm;
+
+import android.app.Application;
+
+/**
+ * Created by AlejandroMissael on 22/04/2017.
+ */
+
+public class baseDatos extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        RealmConfiguration
+
+    }
+}
