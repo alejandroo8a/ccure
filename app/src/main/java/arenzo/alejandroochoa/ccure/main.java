fe2bc69f3ad432ffeaf98e327c2746b1e2e977b5
Alejandro Ochoa <alex.jym13@gmail.com>
2017-04-25 18:19:44 -0500

    Se añadio el centrado del texto ademas de que se agrego la fuente

--- app/src/main/java/arenzo/alejandroochoa/ccure/main.java
+++ app/src/main/java/arenzo/alejandroochoa/ccure/main.java
@@ -2,6 +2,12 @@ package arenzo.alejandroochoa.ccure;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 public class main extends AppCompatActivity {
     //TODO CENTRAR EL TEXTO DEL NAVIGATION BAR, AGREGAR NUEVA FUENTE
@@ -9,5 +15,33 @@ public class main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        centrarTituloActionBar();
+    }
+
+    private void centrarTituloActionBar() {
+        ArrayList<View> textViews = new ArrayList<>();
+
+        getWindow().getDecorView().findViewsWithText(textViews, getTitle(), View.FIND_VIEWS_WITH_TEXT);
+
+        if(textViews.size() > 0) {
+            AppCompatTextView appCompatTextView = null;
+            if(textViews.size() == 1) {
+                appCompatTextView = (AppCompatTextView) textViews.get(0);
+            } else {
+                for(View v : textViews) {
+                    if(v.getParent() instanceof Toolbar) {
+                        appCompatTextView = (AppCompatTextView) v;
+                        break;
+                    }
+                }
+            }
+
+            if(appCompatTextView != null) {
+                ViewGroup.LayoutParams params = appCompatTextView.getLayoutParams();
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                appCompatTextView.setLayoutParams(params);
+                appCompatTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            }
+        }
     }
 }
