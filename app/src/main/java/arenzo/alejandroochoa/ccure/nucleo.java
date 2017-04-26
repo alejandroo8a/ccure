adcac5378025f296b88cadacbf6d898a1b7f973e
Alejandro Ochoa <alex.jym13@gmail.com>
2017-04-25 20:22:35 -0500

    Se agrego el navigation drawer, se crearon todos los fragments que se necesitaran

--- app/src/main/java/arenzo/alejandroochoa/ccure/nucleo.java
+++ app/src/main/java/arenzo/alejandroochoa/ccure/nucleo.java
@@ -1,17 +1,27 @@
 package arenzo.alejandroochoa.ccure;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
+
+import com.mikhaellopez.circularimageview.CircularImageView;
 
 import java.util.ArrayList;
 
-public class nucleo extends AppCompatActivity {
+public class nucleo extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
 
 
@@ -24,8 +34,48 @@ public class nucleo extends AppCompatActivity {
     }
 
     private void cargarElementos(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View headerLayout = navigationView.getHeaderView(0);
+        TextView txtNombreUsuario = (TextView) headerLayout.findViewById(R.id.txtNombreUsuario);
+        TextView txtCorreo = (TextView) headerLayout.findViewById(R.id.txtCorreo);
+        CircularImageView imageProfesor = (CircularImageView) headerLayout.findViewById(R.id.perfil);
+        FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
+        tx.replace(R.id.main_content, new checadas());
+        tx.commit();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
 
+    //SE USA PARA CAPTURAR EL BOTON DE RETROSESO
+    @Override
+    public void onBackPressed() {
+        /*String saber=sharedPreferences.getString("MOSTRAR","1");
+        if(saber.equals("0")){
+            Fragment fragment= new buscar();
+            FragmentManager fragmentManager= getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.main_content,fragment).commit();
+            SharedPreferences.Editor editor=sharedPreferences.edit();
+            editor.putString("MOSTRAR","0");
+            editor.commit();
 
+        }
+        else {
+            new AlertDialog.Builder(this)
+                    .setTitle("Cerrar sesión")
+                    .setMessage("¿Seguro que deseas cerrar sesión?")
+                    .setNegativeButton(android.R.string.no, null)
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface arg0, int arg1) {
+                            finish();
+                        }
+                    }).create().show();
+        }*/
     }
 
     private void centrarTituloActionBar() {
@@ -53,5 +103,31 @@ public class nucleo extends AppCompatActivity {
                 appCompatTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
             }
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Fragment fragment = null;
+        switch (id){
+            case R.id.checadas:
+                fragment = new checadas();
+                break;
+            case R.id.configuración:
+                fragment = new configuracion();
+                break;
+            case R.id.sincronizar:
+                fragment = new sincronizacion();
+                break;
+        }
+        if(R.id.cerrarSesion != id){
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.main_content, fragment).commit();
+        }else{
+            finish();
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
